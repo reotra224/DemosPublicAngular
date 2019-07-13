457bc4899a8997a30904d8a09481a0a2bc9cce7e
Ibrahima Sory TRAORE <ibrahima982011@gmail.com>
2019-07-13 03:44:34 +0000

    Ajout des dépendance dans App module.

--- SpringBootWebSocketFront1/src/app/app.module.ts
+++ SpringBootWebSocketFront1/src/app/app.module.ts
@@ -3,16 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SocketComponent } from './components/socket/socket.component';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { ToastrModule } from 'ngx-toastr';
+import { ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { SocketService } from './services/socket.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SocketComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    ToastrModule.forRoot({ timeOut: 3000 }),
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [SocketService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
