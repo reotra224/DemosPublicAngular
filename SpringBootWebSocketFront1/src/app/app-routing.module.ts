4c1f9f431a4336ab4221a106ea46ddf298372bec
Ibrahima Sory TRAORE <ibrahima982011@gmail.com>
2019-07-13 03:43:13 +0000

    Choix du Compnent Socket comme compnent par defaut

--- SpringBootWebSocketFront1/src/app/app-routing.module.ts
+++ SpringBootWebSocketFront1/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SocketComponent } from './components/socket/socket.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: SocketComponent,
+    pathMatch: "full"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
